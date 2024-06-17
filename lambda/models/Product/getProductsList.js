3bd3d0c03eca10155c8a09133c5bbc92b10b51c8
Tamar <Tamar_Ratiani@epam.com>
2024-06-17 16:33:05 +0400

    remove Authorization from the headers

--- lambda/models/Product/getProductsList.js
+++ lambda/models/Product/getProductsList.js
@@ -2,7 +2,7 @@ exports.handler = async function (event) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    'Access-Control-Allow-Headers': 'Content-Type'
 };
 
 const products = [
